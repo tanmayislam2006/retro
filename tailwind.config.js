ce8650671cf737f978f40083293f7b046e228cf0
Your Name <you@example.com>
2025-03-12 14:42:41 +0600

    setup done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                primary: '#F72C5B',
+                'primary-bg': '#FFEEE6',
+                'header-bg': '#FFF9F4',
+                'footer-bg': '#0',
+                'bordercolor': '#E9E9E9',
+            },
+            fontFamily: {
+                primary: ["Mulish", "sans-serif"],
+            },
+            backgroundImage: {
+                'bg': "url('')",
+            },
+        }
+    }
+}
